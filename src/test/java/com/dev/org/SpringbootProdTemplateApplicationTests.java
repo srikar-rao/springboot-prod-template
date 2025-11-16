db29a422ca337aa5111fd0ff0769006b6f2aaea2
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2025-11-16 09:44:09 -0500

    master commit

---
+++ src/test/java/com/dev/org/SpringbootProdTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProdTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
